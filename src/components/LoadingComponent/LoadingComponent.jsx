10db652b7c90e69612e082b3e72563b9ce64a8ac
Segunmaru Faozan <segunfaozan@gmail.com>
2024-01-14 15:01:39 +0100

    completed the test

--- src/components/LoadingComponent/LoadingComponent.jsx
+++ src/components/LoadingComponent/LoadingComponent.jsx
@@ -6,9 +6,17 @@ import logo from "../../assets/logo-transparent.png";
 const LoadingComponent = () => {
   return (
     <div className="loading__component">
-      <img src={leftCircuit} alt="" className="loading__component--first" />
-      <img src={logo} alt="" className="loading__component--second" />
-      <img src={rightCircuit} alt="" className="loading__component--third" />
+      <img
+        src={leftCircuit}
+        alt="circuit-left"
+        className="loading__component--first"
+      />
+      <img src={logo} alt="logo" className="loading__component--second" />
+      <img
+        src={rightCircuit}
+        alt="circuit-right"
+        className="loading__component--third"
+      />
     </div>
   );
 };
